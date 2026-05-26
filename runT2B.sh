81f8fb95cbd000d247bab8a5513150a56ecb6dba
Jules <Jules.Vandenbroeck@ugent.be>
2026-05-26 11:27:04 +0200

    add max-job-runtime for t2b users

--- runT2B.sh
+++ runT2B.sh
@@ -25,7 +25,7 @@ if [[ $1 == 2024* ]]; then
         -n 100000 \
         --work-area crab_projects/crab_projects_data_2024v15 \
         --input-files inputs \
-        --max-memory 4500 $DRYRUN
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
 
     python3 crab.py \
         -p mc_2024_NANO.py \
@@ -39,7 +39,7 @@ if [[ $1 == 2024* ]]; then
         -n 2 \
         --work-area crab_projects/crab_projects_mc_2024v15 \
         --input-files inputs \
-        --max-memory 4500 $DRYRUN
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
 fi 
 if [[ $1 == 2025* ]]; then
     python crab.py \
@@ -54,7 +54,7 @@ if [[ $1 == 2025* ]]; then
         -n 100000 \
         --work-area crab_projects/crab_projects_data_2025v15 \
         --input-files inputs \
-        --max-memory 4500 $DRYRUN
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
 fi 
 if [[ $1 == 2022* ]]; then
     python crab.py \
@@ -69,7 +69,7 @@ if [[ $1 == 2022* ]]; then
         -n 100000 \
         --work-area crab_projects/crab_projects_data_2022v15 \
         --input-files inputs \
-        --max-memory 4500 $DRYRUN
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
 fi 
 if [[ $1 == 2023* ]]; then
     python crab.py \
@@ -84,5 +84,5 @@ if [[ $1 == 2023* ]]; then
         -n 100000 \
         --work-area crab_projects/crab_projects_data_2023v15 \
         --input-files inputs \
-        --max-memory 4500  $DRYRUN
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
 fi
