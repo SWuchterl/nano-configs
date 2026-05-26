f3f020a326eb70153b055d188156ec7e2a013107
Jules <Jules.Vandenbroeck@ugent.be>
2026-05-26 15:07:50 +0200

    add submission script for T2_CH_CSCS

---
+++ runLXplusT2_CH_CSCS.sh
@@ -0,0 +1,88 @@
+# parse --dryrun flag (remove it from positional args so $1 stays as the year)
+DRYRUN=""
+args=()
+for arg in "$@"; do
+    if [[ "$arg" == "--dryrun" ]]; then
+        DRYRUN="--dryrun"
+    else
+        args+=("$arg")
+    fi
+done
+set -- "${args[@]}"
+
+python3 sampleGeneration.py $1 --user $USER --skip-check
+
+if [[ $1 == 2024* ]]; then
+    python crab.py \
+        -p data_2024_NANO.py \
+        --site T2_CH_CSCS \
+        -o /store/group/cmst3/group/hplushf/NanoTuples/2024/data \
+        -t NanoTuples-uParTv3-parTlepID-NanoAODv15 \
+        -i data/data_2024.conf \
+        -e exe_ULv15_nosel.sh \
+        --num-cores 2 \
+        -s EventAwareLumiBased \
+        -n 100000 \
+        --work-area crab_projects/crab_projects_data_2024v15 \
+        --input-files inputs \
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
+
+    python3 crab.py \
+        -p mc_2024_NANO.py \
+        --site T2_CH_CSCS \
+        -o /store/group/cmst3/group/hplushf/NanoTuples/2024/mc \
+        -t NanoTuples-uParTv3-parTlepID-NanoAODv15 \
+        -i  mc/mc_2024.conf \
+        -e exe_ULv15_nosel.sh \
+        --num-cores 2 \
+        -s FileBased \
+        -n 2 \
+        --work-area crab_projects/crab_projects_mc_2024v15 \
+        --input-files inputs \
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
+fi 
+if [[ $1 == 2025* ]]; then
+    python crab.py \
+        -p data_2025_NANO.py \
+        --site T2_CH_CSCS \
+        -o /store/group/cmst3/group/hplushf/NanoTuples/2025/data \
+        -t NanoTuples-uParTv3-parTlepID-NanoAODv15 \
+        -i data/data_2025.conf \
+        -e exe_ULv15_nosel.sh \
+        --num-cores 2 \
+        -s EventAwareLumiBased \
+        -n 100000 \
+        --work-area crab_projects/crab_projects_data_2025v15 \
+        --input-files inputs \
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
+fi 
+if [[ $1 == 2022* ]]; then
+    python crab.py \
+        -p data_2022_NANO.py \
+        --site T2_CH_CSCS \
+        -o /store/group/cmst3/group/hplushf/NanoTuples/2022/data \
+        -t NanoTuples-uParTv3-parTlepID-NanoAODv15 \
+        -i data/data_2022.conf \
+        -e exe_ULv15_nosel.sh \
+        --num-cores 2 \
+        -s EventAwareLumiBased \
+        -n 100000 \
+        --work-area crab_projects/crab_projects_data_2022v15 \
+        --input-files inputs \
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
+fi 
+if [[ $1 == 2023* ]]; then
+    python crab.py \
+        -p data_2023_NANO.py \
+        --site T2_CH_CSCS \
+        -o /store/group/cmst3/group/hplushf/NanoTuples/2023/data \
+        -t NanoTuples-uParTv3-parTlepID-NanoAODv15 \
+        -i data/data_2023.conf \
+        -e exe_ULv15_nosel.sh \
+        --num-cores 2 \
+        -s EventAwareLumiBased \
+        -n 100000 \
+        --work-area crab_projects/crab_projects_data_2023v15 \
+        --input-files inputs \
+        --max-memory 4500 --max-job-runtime 2750 $DRYRUN
+fi
