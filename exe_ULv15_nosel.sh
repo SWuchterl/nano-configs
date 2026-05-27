fe0dcddfc6367b53e6ec3217247306ec6fbddcf9
Jules <Jules.Vandenbroeck@ugent.be>
2026-05-27 11:30:50 +0200

    add retry of wget in case of failed connection

--- exe_ULv15_nosel.sh
+++ exe_ULv15_nosel.sh
@@ -15,11 +15,11 @@ WORKDIR=$(pwd)
 mkdir -p $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTElectronId/v2/
 mkdir -p $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTMuonId/v2
 
-wget https://github.com/SWuchterl/PhysicsTools-NanoAOD/raw/refs/heads/CMSSW_15_0_17_leptonParT/ParTElectronId/v2/electron_ParT_2024.onnx -O $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTElectronId/v2/electron_ParT_2024.onnx
-wget https://github.com/SWuchterl/PhysicsTools-NanoAOD/raw/refs/heads/CMSSW_15_0_17_leptonParT/ParTElectronId/v2/preprocess.json -O $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTElectronId/v2/preprocess.json
+wget https://github.com/SWuchterl/PhysicsTools-NanoAOD/raw/refs/heads/CMSSW_15_0_17_leptonParT/ParTElectronId/v2/electron_ParT_2024.onnx -O $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTElectronId/v2/electron_ParT_2024.onnx --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/PhysicsTools-NanoAOD/raw/refs/heads/CMSSW_15_0_17_leptonParT/ParTElectronId/v2/preprocess.json -O $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTElectronId/v2/preprocess.json --tries=0 --retry-connrefused --wait=30
 
-wget https://github.com/SWuchterl/PhysicsTools-NanoAOD/raw/refs/heads/CMSSW_15_0_17_leptonParT/ParTMuonId/v2/muon_ParT_2024.onnx -O $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTMuonId/v2/muon_ParT_2024.onnx
-wget https://github.com/SWuchterl/PhysicsTools-NanoAOD/raw/refs/heads/CMSSW_15_0_17_leptonParT/ParTMuonId/v2/preprocess.json -O $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTMuonId/v2/preprocess.json
+wget https://github.com/SWuchterl/PhysicsTools-NanoAOD/raw/refs/heads/CMSSW_15_0_17_leptonParT/ParTMuonId/v2/muon_ParT_2024.onnx -O $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTMuonId/v2/muon_ParT_2024.onnx --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/PhysicsTools-NanoAOD/raw/refs/heads/CMSSW_15_0_17_leptonParT/ParTMuonId/v2/preprocess.json -O $CMSSW_BASE/src/PhysicsTools/NanoAOD/data/ParTMuonId/v2/preprocess.json --tries=0 --retry-connrefused --wait=30
 
 # and some more custom models for boosted jets
 mkdir -p ${CMSSW_BASE}/src/RecoBTag/Combined/data/HLT/GlobalParticleTransformerAK15/V00/
@@ -28,16 +28,16 @@ mkdir -p ${CMSSW_BASE}/src/RecoBTag/Combined/data/MassRegression/ak15/V01c
 mkdir -p ${CMSSW_BASE}/src/RecoBTag/Combined/data/OfflineGlobalParticleTransformerAK15/
 mkdir -p ${CMSSW_BASE}/src/RecoBTag/Combined/data/ParticleNet-MD/ak15/V02d
 
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/HLT/GlobalParticleTransformerAK15/V00/model_ak15_2024.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/HLT/GlobalParticleTransformerAK15/V00/model_ak15_2024.onnx
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/HLT/GlobalParticleTransformerAK15/V00/preprocess.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/HLT/GlobalParticleTransformerAK15/V00/preprocess.json
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/InclParticleTransformer-MD/ak15/V02/model.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/InclParticleTransformer-MD/ak15/V02/model.onnx
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/InclParticleTransformer-MD/ak15/V02/preprocess_corr.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/InclParticleTransformer-MD/ak15/V02/preprocess_corr.json
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/MassRegression/ak15/V01c/particle_net_regression.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/MassRegression/ak15/V01c/particle_net_regression.onnx
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/MassRegression/ak15/V01c/preprocess.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/MassRegression/ak15/V01c/preprocess.json
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/OfflineGlobalParticleTransformerAK15/model_ak15.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/OfflineGlobalParticleTransformerAK15/model_ak15.onnx
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/OfflineGlobalParticleTransformerAK15/preprocess.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/OfflineGlobalParticleTransformerAK15/preprocess.json
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/ParticleNet-MD/ak15/V02d/particle-net.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/ParticleNet-MD/ak15/V02d/particle-net.onnx
-wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/ParticleNet-MD/ak15/V02d/preprocess.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/ParticleNet-MD/ak15/V02d/preprocess.json
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/HLT/GlobalParticleTransformerAK15/V00/model_ak15_2024.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/HLT/GlobalParticleTransformerAK15/V00/model_ak15_2024.onnx --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/HLT/GlobalParticleTransformerAK15/V00/preprocess.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/HLT/GlobalParticleTransformerAK15/V00/preprocess.json --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/InclParticleTransformer-MD/ak15/V02/model.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/InclParticleTransformer-MD/ak15/V02/model.onnx --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/InclParticleTransformer-MD/ak15/V02/preprocess_corr.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/InclParticleTransformer-MD/ak15/V02/preprocess_corr.json --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/MassRegression/ak15/V01c/particle_net_regression.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/MassRegression/ak15/V01c/particle_net_regression.onnx --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/MassRegression/ak15/V01c/preprocess.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/MassRegression/ak15/V01c/preprocess.json --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/OfflineGlobalParticleTransformerAK15/model_ak15.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/OfflineGlobalParticleTransformerAK15/model_ak15.onnx --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/OfflineGlobalParticleTransformerAK15/preprocess.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/OfflineGlobalParticleTransformerAK15/preprocess.json --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/ParticleNet-MD/ak15/V02d/particle-net.onnx -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/ParticleNet-MD/ak15/V02d/particle-net.onnx --tries=0 --retry-connrefused --wait=30
+wget https://github.com/SWuchterl/RecoBTag-Combined-data/raw/refs/heads/dev/CMSSW_15_0_17_nanoV15ExtSkimMore/ParticleNet-MD/ak15/V02d/preprocess.json -O ${CMSSW_BASE}/src/RecoBTag/Combined/data/ParticleNet-MD/ak15/V02d/preprocess.json --tries=0 --retry-connrefused --wait=30
 
 # 1) run the actual nanoAOD step first
 cmsRun -j FrameworkJobReport.xml PSet.py
