0874538a95fe402b65100e0d96cb242647fabe14
Jules <Jules.Vandenbroeck@ugent.be>
2026-07-01 10:23:24 +0200

    add 2018UL config for training sample production

---
+++ mc_2018UL_NANO.py
@@ -0,0 +1,152 @@
+# Auto generated configuration file
+# using:
+# Revision: 1.19
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
+# with command line options: mc_2018UL --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --fileout file:nano.root --conditions 150X_mc2018_realistic_v1 --step NANO --filein file:inMINIAOD.root --era Run2_2018,run2_nanoAOD_106Xv2 --no_exec -n -1
+from PhysicsTools.NanoAOD.nano_cff import nanoAOD_customizeCommon
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+from ParticleNetNtuplizer.ParticleNetNtuplizer.electron_ntuplizer import customize_ntuplizer
+from ParticleNetNtuplizer.ParticleNetNtuplizer.muon_ntuplizer import customize_ntuplizer
+from TopQuarkAnalysis.BFragmentationAnalyzer.customizeAddAll import customizeAddWeights
+from NanoTuples.NanoTuples.nanoTuples_cff import nanoTuples_withAK15
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+from Configuration.AlCa.GlobalTag import GlobalTag
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
+from Configuration.Eras.Modifier_run2_nanoAOD_106Xv2_cff import run2_nanoAOD_106Xv2
+
+process = cms.Process('NANO', Run2_2018, run2_nanoAOD_106Xv2)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('PhysicsTools.NanoAOD.nano_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input=cms.untracked.int32(100),
+    output=cms.optional.untracked.allowed(cms.int32, cms.PSet)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+                            fileNames=cms.untracked.vstring(
+                                'file:miniaod2018.root'),
+                            secondaryFileNames=cms.untracked.vstring()
+                            )
+
+process.options = cms.untracked.PSet(
+    IgnoreCompletely=cms.untracked.vstring(),
+    Rethrow=cms.untracked.vstring(),
+    TryToContinue=cms.untracked.vstring(),
+    accelerators=cms.untracked.vstring('*'),
+    allowUnscheduled=cms.obsolete.untracked.bool,
+    canDeleteEarly=cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules=cms.untracked.bool(True),
+    dumpOptions=cms.untracked.bool(False),
+    emptyRunLumiMode=cms.obsolete.untracked.string,
+    eventSetup=cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs=cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs=cms.untracked.uint32(0)
+    ),
+    fileMode=cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun=cms.untracked.bool(False),
+    holdsReferencesToDeleteEarly=cms.untracked.VPSet(),
+    makeTriggerResults=cms.obsolete.untracked.bool,
+    modulesToCallForTryToContinue=cms.untracked.vstring(),
+    modulesToIgnoreForDeleteEarly=cms.untracked.vstring(),
+    numberOfConcurrentLuminosityBlocks=cms.untracked.uint32(0),
+    numberOfConcurrentRuns=cms.untracked.uint32(1),
+    numberOfStreams=cms.untracked.uint32(0),
+    numberOfThreads=cms.untracked.uint32(1),
+    printDependencies=cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB=cms.optional.untracked.uint32,
+    throwIfIllegalParameter=cms.untracked.bool(True),
+    wantSummary=cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation=cms.untracked.string('mc_2018UL nevts:100'),
+    name=cms.untracked.string('Applications'),
+    version=cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.NANOAODSIMoutput = cms.OutputModule("NanoAODOutputModule",
+                                            compressionAlgorithm=cms.untracked.string(
+                                                'LZMA'),
+                                            compressionLevel=cms.untracked.int32(
+                                                9),
+                                            dataset=cms.untracked.PSet(
+                                                dataTier=cms.untracked.string(
+                                                    'NANOAODSIM'),
+                                                filterName=cms.untracked.string(
+                                                    '')
+                                            ),
+                                            fileName=cms.untracked.string(
+                                                'file:nano.root'),
+                                            outputCommands=process.NANOAODSIMEventContent.outputCommands
+                                            )
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag = GlobalTag(
+    process.GlobalTag, '150X_mc2018_realistic_v1', '')
+
+# Path and EndPath definitions
+process.nanoAOD_step = cms.Path(process.nanoSequenceMC)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.NANOAODSIMoutput_step = cms.EndPath(process.NANOAODSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(
+    process.nanoAOD_step, process.endjob_step, process.NANOAODSIMoutput_step)
+associatePatAlgosToolsTask(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from PhysicsTools.NanoAOD.nano_cff
+
+# call to customisation function nanoAOD_customizeCommon imported from PhysicsTools.NanoAOD.nano_cff
+process = nanoAOD_customizeCommon(process)
+
+# End of customisation functions
+
+
+# Customisation from command line
+
+process.source.delayReadingEventProducts = cms.untracked.bool(False)
+
+# End of customisation functions
+
+
+# Customisation from command line
+
+nanoTuples_withAK15(process)
+
+customizeAddWeights(process, addClassicBFragAndDecay=True,
+                    addClassicCFragAndDecay=True, addMLBfrag=True, addMLHdamp=True, addMLNNLO=True)
+
+
+process.source.delayReadingEventProducts = cms.untracked.bool(False)
+
+process = customize_ntuplizer(process)
+
+process = customize_ntuplizer(process)
+
+
+# Add early deletion of temporary data products to reduce peak memory need
+process = customiseEarlyDelete(process)
+# End adding early deletion
